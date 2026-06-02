85599e1c55a6a9b48cc19a3f9a7f154e21d00f2f
Juan Antonio Rodriguez Reyna <juanrodriguezr2135@gmail.com>
2026-06-01 23:42:36 -0600

    movimiento y recoleccion de objeto

--- Assets/Scripts/AnimateHandOnInput.cs
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -7,20 +7,10 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
 
-    
-    
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Trigger", triggerValue);
-
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
